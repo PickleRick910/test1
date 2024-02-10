d6a66c188eaeca44e9ca2485017b580f6d612eda
MOHAMMED FURQAN <157779501+PickleRick910@users.noreply.github.com>
2024-02-10 04:21:04 -0800

    Update MathUtils.java
    made change to multiply

--- src/main/java/com/testing1/MathUtils.java
+++ src/main/java/com/testing1/MathUtils.java
@@ -7,7 +7,7 @@ public class MathUtils
 	public int subtract(int a,int b){
 	return a-b;}
 	public int multiply(int a,int b){
-	return a*b;}
+	return a-b;}
 	public double divide(int a,int b)
 	{ 
 		if(b==0)
